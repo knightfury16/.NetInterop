921900ecca42826cce527755f8a1445b9fd972e2
knightfury16 <suhaibknight@gmail.com>
2026-01-17 23:32:57 +0600

    Interop in dotnet

--- NativeInterop/Program.cs
+++ NativeInterop/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace NativeInterop;
+﻿using System.Runtime.InteropServices;
+
+namespace NativeInterop;
 
 class Program
 {
+    [DllImport("libmyfunc")]
+    public static extern int AddTwoNumb(int a, int b);
+
+    [DllImport("libmyfunc")]
+    public static extern void Greet(string name, int age);
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine($"heelo {AddTwoNumb(5, 4)}");
+        Greet("cool", 28);
+
     }
 }
