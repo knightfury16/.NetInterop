ab8e9d3713bfe7f8ba14278e801a9cc12b7df3a0
knightfury16 <suhaibknight@gmail.com>
2026-01-19 23:39:51 +0600

    configing nuke

--- build/Build.cs
+++ build/Build.cs
@@ -6,10 +6,12 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 class Build : NukeBuild
 {
@@ -19,10 +21,12 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution;
 
     Target Clean => _ => _
         .Before(Restore)
@@ -39,6 +43,10 @@ class Build : NukeBuild
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(s => s
+                            .SetProjectFile(Solution.GetProject("NativeInterop"))
+                            .SetConfiguration(Configuration)
+                            .EnableNoRestore());
         });
 
 }
